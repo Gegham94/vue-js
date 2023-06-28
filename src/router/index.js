885bffde3a3adca250565a9eb6734c3aefa48707
Арутюнян Артем Ваганович <avarutyunyan@vsk.ru>
2023-06-28 18:26:31 +0400

    add vue-js project

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { layout: "empty"},
+    component: () => import ('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: "main"},
+    component: () => import ('../views/Categories.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
